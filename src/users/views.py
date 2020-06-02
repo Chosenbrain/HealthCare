f27c3d3956b99f7c27f28247310cffcbfe4923a8
Chosenbrain <exactmusty1994@gmail.com>
2020-06-02 17:36:30 +0100

    tweaked new

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Doctor
 
 @login_required
 def profile(request):
-    return render(request, 'pages/profile.html', {})
+    doctor = Doctor.objects.all()
+    image = Doctor.objects.all()
+    email = Doctor.objects.all()
+    bio = Doctor.objects.all()
+    speciality = Doctor.objects.all()
+    context ={
+        'image':image,
+        'email':email,
+        'bio':bio,
+        'speciality':speciality
+    }
+    return render(request, 'pages/profile.html', context)
